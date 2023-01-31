33ad3143f8937b3190011fb2507a6e61d1171a66
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-01-31 15:19:30 +0100

    Aggiunge creazione conn. e seeder table pivot tech

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -8,6 +8,7 @@ use App\Models\Project;
 use App\Models\Type;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Schema;
 
 class ProjectSeeder extends Seeder
 {
@@ -19,7 +20,9 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker)
     {
 
+        Schema::disableForeignKeyConstraints();
         Project::truncate();
+        Schema::enableForeignKeyConstraints();
 
         for($i = 0; $i < 10; $i++){
             $type = Type::inRandomOrder()->first();
---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Technology;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::disableForeignKeyConstraints();
+        Technology::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $technologies = ["Vue JS", "Bootstrap", "Laravel" ,"Docker", "Git"];
+
+        foreach($technologies as $technology){
+            $new_technology = new Technology();
+
+            $new_technology->name = $technology;
+            $new_technology->slug = Str::slug($new_technology->name);
+
+            $new_technology->save();
+        }
+    }
+}
